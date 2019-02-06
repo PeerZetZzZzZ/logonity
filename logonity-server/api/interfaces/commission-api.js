d1ccc4d06f69bb9c3e08237eecf11ca974d4abc1
Przemyslaw Thomann <peerzet3@gmail.com>
2019-02-06 10:27:32 +0100

    another issue fix

--- logonity-server/api/interfaces/commission-api.js
+++ logonity-server/api/interfaces/commission-api.js
@@ -8,21 +8,21 @@ const path = require('path');
 var fs = require('fs');
 
 app.express.post('/api/upload',upload.single('foo'), function(req, res) {
+  try {
   console.log(req.body); // the uploaded file object
   console.log(req.file); // the uploaded file object
-  try {
     watermark.embedWatermark(req.file.path, {'text' : 'Logonity', dstPath: `${req.file.path}_watermark`}, function(err) {
       console.log('Error occurred during embed watermark: ', err);
     });
-  } catch(err) {
-    console.log('z.lapalem: ', err);
-  }
   LogonitySaveRepository.saveLogoProposal(req.body.commissionId, req.body.logoComment, req.body.fileName, req.file.filename, req.file.mimetype, req.file.size,
     (id) => {
       res.send(id);
     }, () => {
       res.sendStatus(500);
     });
+  } catch(err) {
+    console.log('z.lapalem: ', err);
+  }
 });
 
 app.express.get('/api/createCommission', function (req, res)  {
